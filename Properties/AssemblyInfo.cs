ce0294b26e3fac656a494cc89717499b2e9f35a2
Bryce Mecum <petridish@gmail.com>
2020-03-27 20:38:02 -0800

    Update copyright for 2020

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KolthSoft")]
 [assembly: AssemblyProduct("TownCrier")]
-[assembly: AssemblyCopyright("Copyright © KolthSoft 2018")]
+[assembly: AssemblyCopyright("Copyright © KolthSoft 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
